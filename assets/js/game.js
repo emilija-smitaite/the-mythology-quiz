c3170997f38fca7122d0086351353a1437c05c05
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-03-11 11:20:14 +0000

    Indicate current selected answer option

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,26 @@
 //https://opentdb.com/api.php?amount=10&category=20&type=multiple
 
 //---------------------Variables-------------------------
+let play = document.getElementById("play-button");
 const question = document.getElementById("question-area");
-let correctAnswer = "", correctAnswerCount = questionCount = 0, numberOfQuestions = 5;
 const option = document.querySelector(".option");
-console.log(option);
+const _score = document.getElementById("score");
+const _questionsTotal = document.getElementById("questions-total");
+let correctAnswer = "", score = questionsAsked = 0, questionsTotal = 5;
 
-let unusedQuestions = [];
+
+
+//let unusedQuestions = [];
 
 //---------------------Event Listeners-------------------------
-let play = document.getElementById("play-button");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchQuestion();
+    _questionsTotal.textContent = questionsTotal;
+    _score.textContent = score;
+})
+
 //--------------------------Functions--------------------
 function startGame() {
 
@@ -22,16 +33,16 @@ async function fetchQuestion() {
     const APIUrl = "https://opentdb.com/api.php?amount=1&category=20&type=multiple";
     const result = await fetch(`${APIUrl}`);
     const data = await result.json();
-    //console.log(data.results);
     displayQuestion(data.results[0]);
 }
 
+//Taking API data and formatting it to display a question and answers
 function displayQuestion(data) {
     correctAnswer = data.correct_answer;
     let incorrectAnswer = data.incorrect_answers;
     let optionsList = incorrectAnswer;
 
-    //create a new array containing random incorrect answers and then correct answer is added
+    //create a new array containing random incorrect and correct answers
     optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
     
     question.innerHTML = `${data.question}`;
@@ -39,11 +50,20 @@ function displayQuestion(data) {
     ${optionsList.map((option, index) => `
     <li>${index + 1}. <span> ${option} </span></li>`).join("")}
     `;
-
-    
+    pickOption();
 }
-fetchQuestion();
-displayQuestion();
+
+function pickOption() {
+    option.querySelectorAll("li").forEach((options) => {
+        options.addEventListener("click", () => {
+            if(option.querySelector(".selected")){
+                const activeOption = option.querySelector(".selected");
+                activeOption.classList.remove('selected');
+            }
+            options.classList.add("selected");
+        })
+    })
+}
 
 function checkCorrectAnswer() {
 
