19b89116f14e1261528b918e8ddb3efbe7581c7e
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-03-13 16:34:43 +0000

    Edit comments on game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,12 @@
-//https://opentdb.com/api.php?amount=10&category=20&type=multiple
+/* https://opentdb.com/api.php?amount=10&category=20&type=multiple
+Generated API link */
 
 /* Script was created using the following tutorial:
 https://www.youtube.com/watch?v=-cX5jnQgqSM
-I have added some event listeners on my own, added my own styling, 
-adapted some of the functions.
+I have added some event listeners on my own, 
+adapted some of the functions,  added my own styling, 
 */
+
 //---------------------Variables-------------------------
 let play = document.getElementById("play-button");
 const question = document.getElementById("question-area");
@@ -15,103 +17,112 @@ const checkAnswerButton = document.getElementById("check-answer-button");
 const playAgainButton = document.getElementById("play-again-button");
 const outcome = document.getElementById("outcome");
 
-let correctAnswer = "", score = questionsAsked = 0, questionsTotal = 5;
-
-
+let correctAnswer = "",
+  score = (questionsAsked = 0),
+  questionsTotal = 5;
 
 //let unusedQuestions = [];
 
 //---------------------Event Listeners-------------------------
-
-
 document.addEventListener("DOMContentLoaded", () => {
-    fetchQuestion();
-    eventListeners();
-    questionsTotalArea.textContent = questionsTotal;
-    scoreArea.textContent = score;
+  fetchQuestion();
+  eventListeners();
+  questionsTotalArea.textContent = questionsTotal;
+  scoreArea.textContent = score;
 });
 
 function eventListeners() {
-    checkAnswerButton.addEventListener("click", checkCorrectAnswer);
+  checkAnswerButton.addEventListener("click", checkCorrectAnswer);
 }
-
 
 //--------------------------Functions--------------------
 
 //An asynchronous funtion to fetch data from the API
 async function fetchQuestion() {
-    const APIUrl = "https://opentdb.com/api.php?amount=1&category=20&type=multiple";
-    const result = await fetch(`${APIUrl}`);
-    const data = await result.json();
-    outcome.innerHTML = "";
-    displayQuestion(data.results[0]);
+  const APIUrl = "https://opentdb.com/api.php?amount=1&category=20&type=multiple";
+  const result = await fetch(`${APIUrl}`);
+  const data = await result.json();
+  outcome.innerHTML = "";
+  displayQuestion(data.results[0]);
 }
 
 //Taking API data and formatting it to display a question and answers
 function displayQuestion(data) {
-    checkAnswerButton.disabled = false;
-    correctAnswer = data.correct_answer;
-    let incorrectAnswer = data.incorrect_answers;
-    let optionsList = incorrectAnswer;
+  checkAnswerButton.disabled = false;
+  correctAnswer = data.correct_answer;
+  let incorrectAnswer = data.incorrect_answers;
+  let optionsList = incorrectAnswer;
 
-    //create a new array containing random incorrect and correct answers
-    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
-    
-    question.innerHTML = `${data.question}`;
-    option.innerHTML = `
-    ${optionsList.map((option, index) => `
-    <li>${index + 1}. <span>${option}</span></li>`).join("")}
+  //Return array of random answer options
+  optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+
+  question.innerHTML = `${data.question}`;
+
+  //Iterate answer options and place into <li> elements with index prefix
+  option.innerHTML = `
+    ${optionsList
+      .map(
+        (option, index) => `
+    <li>${index + 1}. <span>${option}</span></li>`
+      )
+      .join("")}
     `;
-    pickOption();
+  pickOption();
 }
 
+//Highlight only the answer option clicked on
 function pickOption() {
-    option.querySelectorAll("li").forEach((options) => {
-        options.addEventListener("click", () => {
-            if(option.querySelector(".selected")){
-                const activeOption = option.querySelector(".selected");
-                activeOption.classList.remove('selected');
-            }
-            options.classList.add("selected");
-        })
-    })
+  option.querySelectorAll("li").forEach((options) => {
+    options.addEventListener("click", () => {
+      if (option.querySelector(".selected")) {
+        const activeOption = option.querySelector(".selected");
+        activeOption.classList.remove("selected");
+      }
+      options.classList.add("selected");
+    });
+  });
 }
 
+/*On clicking CHECK ANSWER button check whether answer is correct, increment CORRECT score,
+give user feedback, stop next question being displayed on click without answer selected */
 function checkCorrectAnswer() {
-    checkAnswerButton.disabled = true;
-    if(option.querySelector(".selected")){
-        let pickedAnswer = option.querySelector(".selected span").textContent;
-        if(pickedAnswer == correctAnswer){
-            score++;
-            outcome.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
-        } else {
-            outcome.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer! <p></p><small><b>Correct answer: </b>${correctAnswer}</small></p>`;
-        }
-        incrementQuestionCount();
+  checkAnswerButton.disabled = true;
+  if (option.querySelector(".selected")) {
+    let pickedAnswer = option.querySelector(".selected span").textContent;
+    if (pickedAnswer == correctAnswer) {
+      score++;
+      outcome.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
     } else {
-        outcome.innerHTML = `<p><i class = "fas fa-question"></i>Select your answer!</p>`;
-        checkAnswerButton.disabled = false;
+      outcome.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer! <p></p><small><b>Correct answer: </b>${correctAnswer}</small></p>`;
     }
+    incrementQuestionCount();
+  } 
+   //Handle user clicking CHECK ANSWER button - keep it active until answer is selected
+  else {
+    outcome.innerHTML = `<p><i class = "fas fa-question"></i>Select your answer!</p>`;
+    checkAnswerButton.disabled = false;
+  }
 }
 
+//Increment asked questions count, stop once max number is reached. Provide user feedback on quiz result.
 function incrementQuestionCount() {
-    questionsAsked++;
-    editQuestionCount();
-    if(questionsAsked == questionsTotal) {
-        checkAnswerButton.style.display = "none";
-        outcome.innerHTML += `<p>Your score is ${score}/${questionsTotal}!</p>`
-    } else {
-        setTimeout(() => {
-            fetchQuestion();
-        }, 300);
-    }
+  questionsAsked++;
+  editQuestionCount();
+  if (questionsAsked == questionsTotal) {
+    checkAnswerButton.style.display = "none";
+    outcome.innerHTML += `<p>Your score is ${score}/${questionsTotal}!</p>`;
+  } else {
+    
+    // Delay for better UX
+    setTimeout(() => {
+      fetchQuestion();
+    }, 300);
+  }
 }
 
+//Update relevant DOM elements
 function editQuestionCount() {
-    questionsTotalArea.textContent = questionsTotal;
-    scoreArea.textContent = score;
+  questionsTotalArea.textContent = questionsTotal;
+  scoreArea.textContent = score;
 }
-
-
-
 
