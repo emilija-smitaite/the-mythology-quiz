804a536c7b178c641ccac96a3534cf19b1e58481
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-03-17 14:16:24 +0000

    Fix problem in Email JS, prevent page reload, clear the form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,4 +1,8 @@
-function sendMail(contactForm){
+function sendMail(event){
+ console.log("hi");
+    //Prevent page reload to keep modal active
+    event.preventDefault();
+let contactForm = event.target;
     emailjs.send('service_vqwsw1i', 'template_eruz4r8', {
         'from_name': contactForm.name.value,
         'from_email': contactForm.emailaddress,
@@ -7,6 +11,7 @@ function sendMail(contactForm){
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            contactForm.reset();
         }, 
         function(error) {
             console.log("FAILED", error);
