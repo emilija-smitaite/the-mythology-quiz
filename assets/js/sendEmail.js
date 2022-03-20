f7fb7bfe3c0b87a308a9c524b443ae66f665d3d7
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-03-20 10:13:51 +0000

    Beautify and comment sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,24 +1,26 @@
 let feedback = document.getElementById("contact-feedback");
 
+//EmailJS email delivery
 function sendMail(event) {
-    
-    //Prevent page reload to keep modal active
-    event.preventDefault();
-let contactForm = event.target;
-    emailjs.send('service_vqwsw1i', 'template_eruz4r8', {
-        'from_name': contactForm.name.value,
-        'from_email': contactForm.emailaddress,
-        'message': contactForm.projectsummary.value
+  //Prevent page reload to keep modal active
+  event.preventDefault();
+  let contactForm = event.target;
+  emailjs
+    .send("service_vqwsw1i", "template_eruz4r8", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress,
+      message: contactForm.projectsummary.value,
     })
     .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            feedback.innerHTML = `<p class = "text-success">Form submitted succesfully!</p>`
-            contactForm.reset();
-        }, 
-        function(error) {
-            console.log("FAILED", error);
-            feedback.innerHTML = `<p class = "text-danger">Sorry, there was an error trying to send your message.</p>`
-        });
-        return false;
+      function (response) {
+        console.log("SUCCESS", response);
+        feedback.innerHTML = `<p class = "text-success">Form submitted succesfully!</p>`;
+        contactForm.reset();
+      },
+      function (error) {
+        console.log("FAILED", error);
+        feedback.innerHTML = `<p class = "text-danger">Sorry, there was an error trying to send your message.</p>`;
+      }
+    );
+  return false;
 }
